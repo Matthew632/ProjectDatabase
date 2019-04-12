c630dd23c43ae751b0106e7451c3f50941d10221
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-04-12 13:06:39 +0100

    creates the tables

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS final_project;
+CREATE DATABASE final_project;
+
+DROP DATABASE IF EXISTS final_project;
+CREATE DATABASE final_project;
---
+++ db/tables.sql
@@ -0,0 +1,22 @@
+\c final_project;
+
+CREATE TABLE restaurants (
+    restaurant_id SERIAL PRIMARY KEY,
+    restaurant_name varchar(255) NOT NULL,
+    restaurant_address varchar(255)
+);
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username varchar(255) UNIQUE NOT NULL,
+    first_name varchar(255),
+    last_name varchar(255),
+    restaurant_id INTEGER REFERENCES restaurants(restaurant_id)
+);
+
+CREATE TABLE images (
+    image_id SERIAL PRIMARY KEY,
+    image_path varchar(255),
+    user_id INTEGER REFERENCES users(user_id),
+    restaurant_id INTEGER REFERENCES restaurants(restaurant_id)
+);
